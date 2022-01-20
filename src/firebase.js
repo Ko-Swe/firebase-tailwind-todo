019c023bf686f0ab43fc6b5c1d8bceed1111ac91
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-01-20 22:47:33 +0630

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDCMumI5Nv1D9etaxqcKIhiAbcoZFNyogY",
+  authDomain: "yanmoenaing-dev.firebaseapp.com",
+  projectId: "yanmoenaing-dev",
+  storageBucket: "yanmoenaing-dev.appspot.com",
+  messagingSenderId: "486000610089",
+  appId: "1:486000610089:web:545cda797f12ed1a888538",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
